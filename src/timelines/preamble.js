00089b9f66f22015af7e93c9dcb31285bdc667df
fernandogelin <fernando@fernandogelin.com>
2020-07-29 13:20:11 -0400

    refactor: add different message for at home / cbt task

--- src/timelines/preamble.js
+++ src/timelines/preamble.js
@@ -1,23 +1,25 @@
 import sendCode from '../trials/sendCode'
-import adjustVolume from '../trials/adjustVolume'
+import message from '../trials/message'
 import userId from '../trials/userId'
 import focus from '../trials/focus'
 import experimentStart from '../trials/experimentStart'
-import {AT_HOME} from '../config/main'
+import {AT_HOME, lang} from '../config/main'
 
+const volumeMessage = lang.instructions.adjust_volume
+const makeSure = lang.instructions.make_sure
 const preamble = {
   type: 'html_keyboard_response',
   stimulus: '',
   timeline: (AT_HOME) ?
     [ experimentStart(), 
       userId(), 
-      adjustVolume(),
+      message(`${volumeMessage} \n ${makeSure}`),
       sendCode('open_task', 2000, true)
     ] :
     [ experimentStart(), 
       userId(), 
       focus(), 
-      adjustVolume(),
+      message(volumeMessage),
       sendCode('open_task', 2000, true)
     ]
 }
